d33e565c88fc2febb204de0fd7a7e69b6d545a5c
izharabbasi <izharabbasi101@gmail.com>
2021-06-24 04:21:40 +0500

    compelte challenge 1

--- 01-Fundamentals-Part-1/starter/app.js
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -2,6 +2,20 @@
 // let continent = 'Asia,';
 // let population = '22 Million';
 
+// let isIsland = country;
+// let language;
 
-// console.log(country, continent, population);
+// console.log(typeof isIsland, typeof population, typeof country, typeof language);
 
+let marksheight = 1.88;
+let marksweight = 95;
+
+let johnheight = 1.76;
+let johnweight = 92;
+
+let markBMI = marksweight / marksheight ** 2;
+let johnnBMI = johnweight / johnheight ** 2;
+
+let markHigherBMI = markBMI > johnnBMI;
+
+console.log(markHigherBMI);
