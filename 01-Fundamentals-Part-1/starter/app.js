e7d0d70aab623eea13811a8d83ea5476819226f0
izharabbasi <izharabbasi101@gmail.com>
2021-06-24 20:31:14 +0500

    compelte challenge 3

--- 01-Fundamentals-Part-1/starter/app.js
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -7,15 +7,40 @@
 
 // console.log(typeof isIsland, typeof population, typeof country, typeof language);
 
-let marksheight = 1.88;
-let marksweight = 95;
+// let marksheight = 1.88;
+// let marksweight = 95;
 
-let johnheight = 1.76;
-let johnweight = 92;
+// let johnheight = 1.76;
+// let johnweight = 92;
 
-let markBMI = marksweight / marksheight ** 2;
-let johnnBMI = johnweight / johnheight ** 2;
+// let markBMI = marksweight / marksheight ** 2;
+// let johnnBMI = johnweight / johnheight ** 2;
 
-let markHigherBMI = markBMI > johnnBMI;
 
-console.log(markHigherBMI);
+// if (markBMI < johnnBMI) {
+//     console.log(`Mark's BMI is ${markBMI} lower than John's ${johnnBMI}! `);
+// } else {
+//     console.log('opps');
+// }
+
+const teamDolphin = (97 + 112 + 101) / 3;
+const teamKoalas = (109 + 95 + 106) / 3;
+
+console.log(teamDolphin);
+console.log(teamKoalas);
+
+if(teamDolphin >= 100 && teamDolphin > teamKoalas) {
+    console.log('Team Dolphin Wins');
+} else if (teamDolphin <= 100 && teamDolphin > teamKoalas) {
+    console.log("Team Dolphin wins but their average score less then 100");
+}
+
+if(teamKoalas >= 100 && teamKoalas > teamDolphin) {
+    console.log('Team Koalas Wins');
+} else if (teamKoalas <= 100 && teamKoalas > teamDolphin) {
+    console.log("Team Koalas wins but their average score less then 100");
+} 
+
+if(teamDolphin, teamKoalas  >= 100 && teamDolphin === teamKoalas){
+    console.log('Draw');
+}
